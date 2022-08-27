d65cc8f6ef2e43841465a7903661b51daa4dcc13
oscharko <o.scharkowski@oscharko.de>
2022-08-27 16:09:05 +0200

    Create get quote module closes #5

---
+++ trading-crypto/src/main/java/de/oscharko/Application.java
@@ -0,0 +1,24 @@
+package de.oscharko;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * --------------------
+ * Created by oscharko on 27.08.22 - 10:19 😎
+ * Check out -> www.oscharko.de
+ * --------------------
+ * Camunda-in-Action
+ * Inside the package - udemy.camunda
+ * --------------------
+ */
+@SpringBootApplication
+@EnableProcessApplication
+public class Application 
+{
+    public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
