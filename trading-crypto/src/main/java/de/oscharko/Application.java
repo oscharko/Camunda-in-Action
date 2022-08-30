09235860d2d5b01171037053f37a7ad2349ee1ef
oscharko <o.scharkowski@oscharko.de>
2022-08-30 18:33:17 +0200

    Revision the complete code closes #10

--- trading-crypto/src/main/java/de/oscharko/Application.java
+++ trading-crypto/src/main/java/de/oscharko/Application.java
@@ -16,9 +16,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableProcessApplication
-public class Application 
-{
+public class Application {
     public static void main(String... args) {
-		SpringApplication.run(Application.class, args);
-	}
+        SpringApplication.run(Application.class, args);
+    }
 }
