34f9102c92c5252c265a17c51659a95a1f71cea3
oscharko <o.scharkowski@oscharko.de>
2022-08-30 18:25:43 +0200

    Create and implement a notification service closes #9

---
+++ trading-crypto/src/main/java/de/oscharko/NotificationService.java
@@ -0,0 +1,16 @@
+package de.oscharko;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution ctx) throws Exception {
+
+        System.out.println("Notification : " + ctx.getVariable("message"));
+
+    }
+}
